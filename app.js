9453581a5ecf0113ee129cc225ade0f6ed15f10c
Radjartha <radjartha12@gmail.com>
2023-10-24 17:33:00 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const db = require("./db.json");
+const app = express();
+
+
+
+
+// Define a route to handle GET requests
+
+
+app.get("/projectsdetail", (req, res) => {
+  res.json(db.projectsdetail);
+});
+
+app.get("/developer", (req, res) => {
+  res.json(db.developer);
+});
+// Start the Express server
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
