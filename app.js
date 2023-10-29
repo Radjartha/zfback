e08025c548ec2b4c56267f4b84e028a864bff196
Radjartha <radjartha12@gmail.com>
2023-10-29 18:18:16 +0700

    test  api

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
+
 const express = require("express");
 var cors = require('cors');
 const db = require("./db.json");
 const app = express();
-
+var fs = require('fs'); //require file system object
 
 
 
 // Define a route to handle GET requests
 app.use(cors());
+
+app.get('/projectsdetail/:id', (req, res) => {
+  const projectId = req.params.id; // Extract the 'id' parameter from the URL
+  const projectDetail = db.projectsdetail[projectId - 1 ]; // Assuming 'db.projectsdetail' is an array or object containing project details
+
+  if (projectDetail) {
+    res.json(projectDetail); // Send the project detail as JSON response
+  } else {
+    res.status(404).json({ error: 'Project not found' }); // Send a 404 error if project is not found
+  }
+});
+
 
 app.get("/projectsdetail", (req, res) => {
   res.json(db.projectsdetail);
@@ -16,6 +29,8 @@ app.get("/projectsdetail", (req, res) => {
 app.get("/developer", (req, res) => {
   res.json(db.developer);
 });
+
+
 // Start the Express server
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
